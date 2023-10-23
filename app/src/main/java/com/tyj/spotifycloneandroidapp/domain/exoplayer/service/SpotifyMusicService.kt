c062d829c98d49ef79d87fa8996289a2ad3933ee
Yen-Ju Tseng <ytseng@ucsd.edu>
2023-10-23 01:16:39 -0500

    combine SpotifyMusicService and the SpotifyMusicNotification

--- app/src/main/java/com/tyj/spotifycloneandroidapp/domain/exoplayer/service/SpotifyMusicService.kt
+++ app/src/main/java/com/tyj/spotifycloneandroidapp/domain/exoplayer/service/SpotifyMusicService.kt
@@ -1,8 +1,12 @@
 package com.tyj.spotifycloneandroidapp.domain.exoplayer.service
 
+import android.content.Intent
+import android.os.Build
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import com.tyj.spotifycloneandroidapp.domain.exoplayer.notification.SpotifyMusicNotificationManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -11,6 +15,20 @@ class SpotifyMusicService: MediaSessionService() {
 
     @Inject
     lateinit var mediaSession: MediaSession
+
+    @Inject
+    lateinit var notificationManager: SpotifyMusicNotificationManager
+
+    @UnstableApi
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.startNotificationService(
+                mediaSession = mediaSession,
+                mediaSessionService = this
+            )
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession {
         return mediaSession
     }
